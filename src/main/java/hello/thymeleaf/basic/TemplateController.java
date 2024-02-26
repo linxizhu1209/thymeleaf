fd48c55466e3a59872be23343754b2fd166d3385
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-02-26 11:59:15 +0900

    [템플릿 레이아웃 1] - ::title, ::link 같이 설정하면, 현재 페이지의 title 및 link 태그들을 레이아웃에 전달한다.

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -13,6 +13,10 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
+    @GetMapping("layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
 
 
 
